a0a1cb538f89a13b5ab7165b7b882feb038ad452
ppp0s <97409880+ppp0s@users.noreply.github.com>
2022-09-05 15:07:08 +0800

    Update Ex05.java

--- Ex05.java
+++ Ex05.java
@@ -4,14 +4,31 @@ public class Ex02 {
 
 	public static void main(String[] args) {
 		// TODO 自動產生的方法 Stub
+		
+		System.out.println(6<<2);
+		// 6的2進制:00000110  
+			// '00001100' 6<<1  : 12
+			// '00011000' 6<<2  : 24
+		
 		Data data =new Data();
 		data.sum(80,70,60);
+		int chn,eng,math;
+		
+		/*自行輸入 國文,英文,數學 成績
+		Scanner sc = new Scanner(System.in);
+		System.out.println("輸入三科成績(國文,英文,數學):");
+		chn = sc.nextInt();
+		eng = sc.nextInt();
+		math = sc.nextInt();
+		
+		data.sum(chn,eng,math);
 		data.print();
+		*/
 		
 		int mile = 800;
 		System.out.println("\n里程數:"+mile+" 車資:"+data.taxi(mile));
 		mile =2200;
-		System.out.println("里程數:"+mile+" 車資:"+data.taxi(mile));				
+		System.out.println("里程數:"+mile+" 車資:"+data.taxi(mile));			
 	}
 }
 
