fa3cf2e2909227f733eb7db22b69b4e5061e02a6
ppp0s <97409880+ppp0s@users.noreply.github.com>
2022-09-05 16:33:20 +0800

    Update Ex05.java

--- Ex05.java
+++ Ex05.java
@@ -53,8 +53,38 @@ public class Data {
     System.out.println("總分:"+total());
 	System.out.println("平均:"+avg());
 	}
-	
+//
 	int taxi(int mile){
 		int money=((mile-1000)/300)*10+80;
 		return mile<1000 ?80:money;
 	}
+//
+	double setPrice(int price) {
+		if(price>1000) {
+			price*=0.8;
+		}
+		return price;
+	}
+	
+	double tax(int income) {
+		double total=0;
+		if(income<=300000) {
+			total =income*0.06;
+		}else if (income>300000) {
+			total = income*0.06+ (income%300000)*0.13;
+		}		return total;
+	}
+	
+	double setshop(int price) {
+		if(price>5000) {
+			price*=0.7;
+		}else if(price >=4000){
+			price*=0.8;
+		}else if(price >=3000) {
+			price*=0.9;
+		}
+		return price;
+	}
+	
+}
+	
