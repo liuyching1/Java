7727a21712cad356649ec63ef27dcb9ce7371049
ppp0s <97409880+ppp0s@users.noreply.github.com>
2022-09-05 17:19:39 +0800

    Create Ex01.java

---
+++ 0902/Ex01.java
@@ -0,0 +1,34 @@
+/*-Ex01.java-*/
+package top01;
+
+public class Ex01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ppp0");
+
+		int i1=1;
+		System.out.println("整數(1):\ni1:"+i1);
+	
+		boolean b1=true;
+		boolean b2=false;
+		System.out.println("布林(2):\nb1:"+b1);
+		System.out.println("b2:"+b2);
+		
+		String s1="123",s2="456";
+		System.out.println("字串(3):\ns1+S2:" +s1+s2);
+		
+		float f1= 10;
+		double d1 =10;
+		System.out.println("浮點數(4):\nf1:"+f1);
+		System.out.println("d1:"+d1);
+		
+		int math=100,chn=95,eng=90;
+		int sum=math+chn+eng;
+		double avg=sum/3;
+		System.out.println("計算平均(5):\navg:"+ avg);
+		
+		char c='c';
+		System.out.println("輸出字元(6):\nc:"+ c);
+		
+	}
