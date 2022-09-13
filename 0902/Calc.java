8ec0365ef68e2f2d32231da927a2f478d60290c4
ppp0s <97409880+ppp0s@users.noreply.github.com>
2022-09-13 15:14:01 +0800

    Create Calc.java

---
+++ 0902/Calc.java
@@ -0,0 +1,29 @@
+public class Calc {
+	double result;
+
+	double plus(double a, double b) {
+		result = a + b;
+		return result;
+	}
+
+	double sub(double a, double b) {
+		result = a - b;
+		return result;
+	}
+
+	double mult(double a, double b) {
+		result = a * b;
+		return result;
+	}
+
+	double div(double a, double b) {
+		result = a / b;
+		return result;
+	}
+
+	double per(double a, double b) {
+		result = a % b;
+		return result;
+	}
+
+}
