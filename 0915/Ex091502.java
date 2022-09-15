b315126dc37869e1d27c75871d9fce0b5b0dba07
ppp0s <97409880+ppp0s@users.noreply.github.com>
2022-09-15 12:20:28 +0800

    Update Ex091502.java

--- 0915/Ex091502.java
+++ 0915/Ex091502.java
@@ -7,30 +7,45 @@ print:
 */
 package top03;
 
+import java.util.Random;
+
 public class Ex091502 {
 
 	public static void main(String[] args) {
 		// TODO 自動產生的方法 Stub
+
 		int[][] ii = { { 1, 5, 2 }, { 8, 5, 9 }, { 3, 7, 7 } };
 		int[] ySumi = new int[ii.length];
-		System.out.println("ii內容:");
-		int xSum = 0, ySum = 0, i, j;
+		System.out.println("\n\nii內容:");
+		int xSumi = 0, ySum = 0, i, j;
 		for (i = 0; i < ii.length; i++) {
-			xSum = 0;
+			xSumi = 0;
 			for (j = 0; j < ii[i].length; j++) {
-				xSum += ii[i][j];
+				xSumi += ii[i][j];
 				ySumi[i] += ii[j][i];
 				System.out.print(ii[i][j] + "  ");
 
 			}
-			ySum += xSum;
-			System.out.print(xSum);
+			ySum += xSumi;
+			System.out.print(xSumi);
 			System.out.println();
 		}
 		for (int print : ySumi) {
 			System.out.print(print + " ");
 		}
 		System.out.println(ySum);
+		/*****/
+		
+		int[][] jj = new int[3][3];
+		int x = 0, y = 0, num = 9;
+		System.out.println("\n\njj內容:");
+		for (x = 0; x < jj.length; x++) {
+			for (y = 0; y < jj[x].length; y++) {
+				jj[x][y] = (int) Math.floor(Math.random() * num + 1);
+				System.out.print(jj[x][y] + " ");
+			}
+			System.out.println();
+		}
 	}
 
 }
