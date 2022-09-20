894ca57e576e56e1952bfb6df9da771ecbcbd5e0
ppp0s <97409880+ppp0s@users.noreply.github.com>
2022-09-20 14:20:14 +0800

    Create EX092003.java

---
+++ 0920/EX092003.java
@@ -0,0 +1,87 @@
+package top04;
+
+public class EX092003 {
+
+	public static void main(String[] args) {
+		// TODO 自動產生的方法 Stub
+		Child chd = new Child();
+		System.out.println(chd.age);
+	//	chd.mark();
+	//	chd.print();
+		
+		System.out.println(chd.whoclass);
+		System.out.println(((Father)chd).whoclass);
+		chd.myname(); //方法若在子類別可用則用, 若無則用父類別方法
+		
+		System.out.println(((Father)chd).age);
+	}
+
+}
+
+
+package top04;
+
+public class Father {
+	public String age = "55";
+	public String name = "FatherName";
+	public String id = "Id .F";
+	public String whoclass = "Father";
+
+	Father() { // 預設建構子
+		this.age = "54";
+		this.name = "isJohn";
+	}
+
+	Father(String f) { // 自訂父類別建構子
+        // this.age="54";
+		// this.name="isJohn";
+	}
+
+	public void myname() {
+		System.out.println(whoclass);
+	}
+
+}
+
+//多載(Overload) : 方法名稱相同, 簽名不相同
+//覆寫(Override) : 方法名稱相同, 簽名相同
+class Child extends Father {
+
+	public String deptName = "deptName"; // 部門名稱
+	public String course = "course";
+	public String whoclass = "Child";
+
+	Child() { // 預設建構子
+
+	}
+	
+	Child(String c) { // 自訂子類別建構子
+		
+	}
+	public void myname() {
+		System.out.println(whoclass);
+	}
+
+	/*
+	 * 
+	 * public void print() { System.out.println("部門是：" + deptName + "，任教科目：" +
+	 * course); } public void mark() { System.out.println("列印學生的成績"); }
+	 */
+}
+
+/*----------
+
+public int fa() {
+	return 1;
+}
+
+public int fa(int fa) {
+	return fa;
+}
+
+
+public int fa() {
+	return 2;
+}
+
+*/
