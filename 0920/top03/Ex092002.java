5a98a2549336608331c330c49c8217656f83e242
ppp0s <97409880+ppp0s@users.noreply.github.com>
2022-09-20 22:07:08 +0800

    Update Ex092002.java

--- 0920/top03/Ex092002.java
+++ 0920/top03/Ex092002.java
@@ -13,27 +13,3 @@ public class Ex092002 {
 	}
 
 }
-
-class GuessNum {
-	private int ans;
-
-	public int getAns() {
-		return ans;
-	}
-
-	public void setAns() {
-		this.ans = (int) Math.floor(Math.random() * 10 + 1);
-	}
-
-	public String guess(int myAns) {
-		String result = "";
-		if (ans > myAns) {
-			result = "答案是:" + ans + ", 你的答案是:" + myAns + ", 你猜太小了!";
-		} else if (ans < myAns) {
-			result = "答案是:" + ans + ", 你的答案是:" + myAns + ", 你猜太大了!";
-		} else {
-			result = "答案是:" + ans + ", 你猜對了!";
-		}
-		return result;
-	}
-}
