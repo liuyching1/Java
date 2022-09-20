791a6bdfe0a04e937ae14b8f412274e99cca69d4
ppp0s <97409880+ppp0s@users.noreply.github.com>
2022-09-20 20:08:50 +0800

    Update Ex090202.java

--- 0902/top01/Ex090202.java
+++ 0902/top01/Ex090202.java
@@ -15,30 +15,3 @@ public class Ex090202 {
 		System.out.println("稅率:" + r + "\n薪資:" + s + "\n所得:" + t);
 	}
 }
-
-
-/*-CalcTax.java-*/
-public class CalcTax {
-	void calc() { // 計算所得
-		int salary = 30000;
-		double rate = 0.06;
-		double tax = salary * rate;
-		System.out.println("稅率:" + rate + "\n薪資:" + salary + "\n所得:" + tax + "\n");
-	}
-
-	void calc01(int salary) {
-		double rate = 0.06;
-		double tax = salary * rate;
-		System.out.println("稅率:" + rate + "\n薪資:" + salary + "\n所得:" + tax + "\n");
-	}
-
-	void calc02(int salary, double rate) {
-		double tax = salary * rate;
-		System.out.println("稅率:" + rate + "\n薪資:" + salary + "\n所得:" + tax + "\n");
-	}
-
-	double calc03(int salary, double rate) { // 傳回'double'型別資料
-		double tax = salary * rate;
-		return tax;
-	}
-}
