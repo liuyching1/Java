e531ca4ef907821c097b155bb9c1e8f268da9235
ppp0s <97409880+ppp0s@users.noreply.github.com>
2022-09-20 16:12:07 +0800

    Create overload-ride.java

---
+++ 0920/overload-ride.java
@@ -0,0 +1,66 @@
+package top04;
+
+public class ExtendsEx {
+
+	public static void main(String[] args) {
+		// TODO 自動產生的方法 Stub
+		Num iarr = new Num();
+		int n = 5;
+		/*		
+		iarr.setNum(19);
+		iarr.serName("Mr.J");
+		iarr.getNum();
+		iarr.getName();
+		System.out.println(iarr.getNum());
+		System.out.println(iarr.getName());
+		 */	
+	}
+
+}
+
+
+
+
+////
+public class Num {
+	private int num;
+	private String name;
+
+	Num() {
+		//this.name = "myname";
+		//this.num = 19;
+	}
+	
+	//多載Overload: 方法名稱可相同, 簽名須不同(參數個數,型別), 回傳值不影響
+	int m() {
+		return 0;
+	}
+	
+	int m(int m) {
+		return m;
+	}
+	
+	int m(byte m,int n) {
+		return m;
+	}
+	
+	//覆寫Override:
+	
+	/*
+	public int getNum() {
+		return num;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setNum(int Num) {
+		this.num = Num;
+	}
+
+	public void serName(String Name) {
+		this.name = Name;
+	}*/
+
+}
