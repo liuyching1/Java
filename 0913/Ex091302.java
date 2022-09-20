8ef231592993317d20e3dd495ace3b1e92985e4e
ppp0s <97409880+ppp0s@users.noreply.github.com>
2022-09-20 21:45:36 +0800

    Update Ex091302.java

--- 0913/Ex091302.java
+++ 0913/Ex091302.java
@@ -44,39 +44,3 @@ public class Ex091302 {
 
 	}
 }
-public class Data {
-	/*----找關鍵字----*/
-	boolean equalsStr(String[] Str, String keyword) {
-		boolean isfalse = false;
-		for (String sEx : Str) {
-			if (sEx == keyword) {
-				System.out.print("關鍵字:'" + keyword + "'已找到，");
-				isfalse = true;
-				System.out.print("結果為:");
-				break;
-			}
-		}
-		return isfalse;
-	}
-	/*----取最大值----*/
-	int getmax(int[] numArr) {
-		int max = 0;
-		for (int num : numArr) {
-			if (num > max) {
-				max = num;
-			}
-		}
-		return max;
-	}
-	/*----取最小值----*/
-	int getmin(int[] numArr) {
-		int min = numArr[0];
-		for (int num : numArr) {
-			if (min > num) {
-				min = num;
-			}
-		}
-		return min;
-	}
-
-}
