46bf9d67f2d555dfbdf46275803ef5d312d8db2f
ppp0s <97409880+ppp0s@users.noreply.github.com>
2022-09-20 21:05:41 +0800

    Update and rename Ex090805.java to Ex090803.java

---
+++ 0908/Ex090803.java
@@ -0,0 +1,88 @@
+package top02;
+
+import java.util.Scanner;
+
+public class Ex090803 {
+
+	public static void main(String[] args) {
+		// TODO 自動產生的方法 Stub
+		
+		int sum=0;
+		for(int i=1;i<=10;i++) {
+			sum+=i;
+		}
+		System.out.println("1~10相加總合:"+sum);
+		sum=0; //歸零
+		
+		int n=1;
+		while(n<=10) {
+			sum+=n;
+			n++;
+		}
+		System.out.println("1~10相加總合:"+sum);
+		sum=0; //歸零
+		
+		n =1;
+		do{
+		sum+=n;
+		n++;
+		}while(n<=10);
+		System.out.println("1~10相加總合:"+sum);
+		sum=0; //歸零
+		
+		Data01 d01 = new Data01();
+		int a=1,b=100,c=10;
+		System.out.print("for迴圈...");
+		System.out.print("\n1~100列出5的倍數:");
+		d01.getnum();
+		System.out.print("\n1~100列出"+c+"的倍數:");
+		d01.getnum(c);
+		System.out.print("\n"+a+"~"+b+"列出"+c+"的倍數:");
+		d01.getnum(a,b,c);
+		System.out.print("\nwhile迴圈...");
+		System.out.print("\n10~100列出"+c+"的倍數:");
+		d01.getwhilenum();
+		
+		/* static funtion */
+		Data01.intsum(2,10);     
+		
+		/*- 猜亂數-*/
+		double rNum =Math.floor(Math.random()*5+1);
+		System.out.println("產生亂數:"+(int)rNum);
+		
+		Scanner  sc = new Scanner(System.in);	
+		boolean trueOrfalse = false;
+		int  myNum;
+		
+		while(!trueOrfalse) {
+			
+			myNum = sc.nextInt();
+			trueOrfalse=d01.result(rNum,myNum);
+			if(trueOrfalse) {
+				System.out.println("亂數:"+(int)rNum+"你猜的數字"+myNum);
+				System.out.println("你猜對了~Bye~");
+				trueOrfalse=true;
+				}
+			else {
+				System.out.println("亂數:"+(int)rNum+"你猜的數字"+myNum);
+				trueOrfalse=false;
+			}
+			
+		}
+
+	
+		int p=0;
+		
+		// print 01 11 12 23 35  58
+		// print  1 2  3  5   8  13 
+		/*
+		for(int i=1,j=1;i<=10;i++) {
+			System.out.print(p+i+" ");	
+			p+=i;
+			//n+=i;
+			//System.out.print(n+" ");
+		}	
+		 */
+	}
+
+}
