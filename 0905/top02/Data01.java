1a0fabdb37185102d72ce4fd99ce7b9412e62895
ppp0s <97409880+ppp0s@users.noreply.github.com>
2022-09-20 20:48:35 +0800

    Update Data01.java

--- 0905/top02/Data01.java
+++ 0905/top02/Data01.java
@@ -1,53 +1,64 @@
 package top02;
 
-public class Data01 {
-	void getnum() {
-		int sum = 0;
-		for (int i = 1; i <= 100; i++) {
-			if (i % 5 == 0)
-				System.out.print(i + " ");
-		}
+public class Data {
+	int chn, eng, math;
+	int sum;
+
+	void sum(int a, int b, int c) {
+		chn = a;
+		eng = b;
+		math = c;
 	}
 
-	void getnum(int a) {
-		int sum = 0;
-		for (int i = 1; i <= 100; i++) {
-			if (i % a == 0)
-				System.out.print(i + " ");
-		}
+	int total() {
+		return chn + eng + math;
 	}
 
-	void getnum(int begin, int end, int num) {
-		int sum = 0;
-		for (int i = begin; i <= end; i++) {
-			if (i % num == 0)
-				System.out.print(i + " ");
-		}
+	double avg() {
+		return total() / 3;
 	}
 
-	void getwhilenum() {
-		int sum = 0;
-		int i = 1;
-		while (i <= 100) {
-			if (i % 10 == 0) {
-				System.out.print(i + " ");
-			}
-			i++;
-		}
+	void print() {
+		System.out.println("國文:" + chn);
+		System.out.println("英文:" + eng);
+		System.out.println("數學:" + math);
+		System.out.println("總分:" + total());
+		System.out.println("平均:" + avg());
 	}
 
-	static void intsum(int... num) {
-		int sum = 0;
-		System.out.println();
-		for (int i = 0; i < num.length; i++) {
-			sum += num[i];
-		}
-		System.out.println("int...加總:" + sum);
+//
+	int taxi(int mile) {
+		int money = ((mile - 1000) / 300) * 10 + 80;
+		return mile < 1000 ? 80 : money;
 	}
 
-	boolean result(double rNum, int myNum) {
-		if (myNum == rNum)
-			return true;
-		return false;
+//
+	double setPrice(int price) {
+		if (price > 1000) {
+			price *= 0.8;
+		}
+		return price;
 	}
+
+	double tax(int income) {
+		double total = 0;
+		if (income <= 300000) {
+			total = income * 0.06;
+		} else if (income > 300000) {
+			total = income * 0.06 + (income % 300000) * 0.13;
+		}
+		return total;
+	}
+
+	double setshop(int price) {
+		if (price > 5000) {
+			price *= 0.7;
+		} else if (price >= 4000) {
+			price *= 0.8;
+		} else if (price >= 3000) {
+			price *= 0.9;
+		}
+		return price;
+	}
+
 }
